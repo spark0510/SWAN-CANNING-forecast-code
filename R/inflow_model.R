5ce273c0caf7f6e757c96532f7f7777eb4ef9a69
addelany <addelany@gmail.com>
2024-02-13 16:05:49 -0600

    update inflow model data and code

--- R/inflow_model.R
+++ R/inflow_model.R
@@ -53,15 +53,17 @@ df_combined <- bind_rows(df_future, df_past) |>
 
 forecast_precip <- df_combined |> 
   filter(variable == 'precipitation') |> 
+  summarise(precip_hourly = median(prediction, na.rm = TRUE), .by = c("datetime")) |> 
   mutate(date = lubridate::as_date(datetime)) |> 
-  summarise(precip = sum(prediction, na.rm = TRUE), .by = c("date")) |> 
+  summarise(precip = sum(precip_hourly, na.rm = TRUE), .by = c("date")) |> 
   mutate(sevenday_precip = RcppRoll::roll_sum(precip, n = 7, fill = NA,align = "right")) |> 
   mutate(doy = lubridate::yday(date))
 
 forecast_temp <- df_combined |> 
   filter(variable == 'temperature_2m') |> 
+  summarise(temp_hourly = median(prediction, na.rm = TRUE), .by = c("datetime")) |> 
   mutate(date = lubridate::as_date(datetime)) |> 
-  summarise(temperature = mean(prediction, na.rm = TRUE), .by = c("date"))
+  summarise(temperature = median(temp_hourly, na.rm = TRUE), .by = c("date"))
 
 forecast_met <- forecast_precip |> 
   right_join(forecast_temp, by = c('date'))
@@ -108,35 +110,135 @@ forecast_drivers <- forecast_met |>
   left_join(site_inflow_wide, by = c('date')) |> 
   drop_na(total_flow)
 
-split <- initial_split(forecast_drivers, prop = 0.80, strata = NULL)
+# split <- initial_split(forecast_drivers, prop = 0.80, strata = NULL)
+# 
+# train_data <- training(split)
+# test_data <- testing(split)
 
-train_data <- training(split)
-test_data <- testing(split)
+train_data <- forecast_drivers |> 
+  filter(date < Sys.Date())
+
+## define folds in training data 
+folds <- vfold_cv(train_data, v = 10)
 
 #set the recipe
 rec <- recipe(total_flow ~ precip + sevenday_precip + doy + temperature,
               data = train_data)
 
 rec_preprocess <- rec |> 
-  step_normalize(all_numeric_predictors())
+  step_normalize(all_numeric_predictors()) #|> 
+  #step_dummy(doy)
 
-xgboost_mod <- boost_tree() |> 
+## define model and tunining parameters
+xgboost_mod <- boost_tree(tree_depth = tune(), trees = tune(), learn_rate = tune()) |> 
   set_mode("regression") |>  
   set_engine("xgboost")
 
+# define the model workflow
 xgboost_inflow_wkflow <- 
   workflow() %>% 
   add_model(xgboost_mod) %>% 
   add_recipe(rec_preprocess)
 
+# tune the hyper-parameters
+inflow_resample_fit <- xgboost_inflow_wkflow |> 
+  tune_grid(resamples = folds, 
+            grid = 25, 
+            control = control_grid(save_pred = TRUE),
+            metrics = metric_set(rmse))
+
+# show the results from tuning 
+inflow_resample_fit %>% 
+  collect_metrics() |> 
+  arrange(mean)
+
+best_hyperparameters <- inflow_resample_fit %>%
+  select_best("rmse")
+
+final_wrorkflow <- xgboost_inflow_wkflow |> 
+  finalize_workflow(best_hyperparameters)
+
 ## fit the model
-xgboost_inflow_fit <- fit(xgboost_inflow_wkflow, data = train_data)
+xgboost_inflow_fit <- fit(final_wrorkflow, data = train_data)
 
-## set up forecasted df for predictions
-predicted_met <- forecast_met |> 
-  filter(date >= Sys.Date())
+# make predictions for each enemble member 
+forecast_precip_ens <- df_combined |> 
+  filter(variable == 'precipitation') |> 
+  #summarise(precip_hourly = sum(prediction, na.rm = TRUE), .by = c("datetime","ensemble")) |> 
+  mutate(date = lubridate::as_date(datetime)) |> 
+  summarise(precip = sum(prediction, na.rm = TRUE), .by = c("date","ensemble")) |> 
+  arrange(date, ensemble) |> 
+  group_by(ensemble) |> 
+  mutate(sevenday_precip = RcppRoll::roll_sum(precip, n = 7, fill = NA,align = "right")) |> 
+  ungroup() |> 
+  mutate(doy = lubridate::yday(date))
 
-# make future inflow predictions
-inflow_predictions <- predict(xgboost_inflow_fit, new_data = predicted_met)
+forecast_temp_ens <- df_combined |> 
+  filter(variable == 'temperature_2m') |> 
+  mutate(date = lubridate::as_date(datetime)) |> 
+  summarise(temperature = median(prediction, na.rm = TRUE), .by = c("date","ensemble"))
 
-prediction_df <- cbind(predicted_met,inflow_predictions)
+forecast_met_ens <- forecast_precip_ens |> 
+  right_join(forecast_temp_ens, by = c('date',"ensemble")) |> 
+  arrange(date,ensemble)
+
+#make empty dataframe to store predictions
+data_build <- data.frame()
+
+for (i in unique(forecast_met_ens$ensemble)){
+  
+  ens_df <- forecast_met_ens |> 
+    filter(ensemble == i)
+  
+  ens_inflow <- predict(xgboost_inflow_fit, new_data = ens_df)
+  
+  ens_predictions <- cbind(ens_df,ens_inflow) |> 
+    rename(prediction = .pred)
+  
+  
+  data_build <- bind_rows(data_build,ens_predictions)
+  
+}
+
+final_predictions <- data_build 
+
+write.csv(final_predictions, 'inflow_predictions.csv', row.names = FALSE)
+
+### forecasting for day instead of each ensemble member
+# ## set up forecasted df for predictions
+# predicted_met <- forecast_met |> 
+#   filter(date >= Sys.Date())
+
+# # make future inflow predictions
+# inflow_predictions <- predict(xgboost_inflow_fit, new_data = predicted_met)
+# 
+# prediction_df <- cbind(predicted_met,inflow_predictions)
+
+# # ## visual inspection using training data
+# historical_drivers <- forecast_drivers |>
+#   filter(date < Sys.Date())
+# 
+# trained_predictions <- predict(xgboost_inflow_fit, new_data = historical_drivers)
+# 
+# historical_predictions <- cbind(historical_drivers, trained_predictions) |>
+#   rename(predictions = .pred)
+# 
+# inflow_plot <- ggplot(historical_predictions, aes(x=date)) +
+#   geom_line(aes(y = total_flow), color = "darkred") +
+#   geom_line(aes(y = predictions), color="steelblue") +
+#   xlim(c(as.Date('2021-01-01'), as.Date('2022-01-01')))
+# 
+# inflow_plot
+# 
+# ## testing set
+# test_predictions <- predict(xgboost_inflow_fit, new_data = test_data)
+# 
+# test_predictions <- cbind(test_data, test_predictions) |>
+#   rename(predictions = .pred)
+# 
+# inflow_plot <- ggplot(test_predictions, aes(x=date)) +
+#   geom_line(aes(y = total_flow), color = "darkred") +
+#   geom_line(aes(y = predictions), color="steelblue") #+
+#   #xlim(c(as.Date('2021-01-01'), as.Date('2022-01-01')))
+# 
+# inflow_plot
